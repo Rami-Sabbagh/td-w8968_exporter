1f81759489d9414b9d1915817a061ece9f819f16
Rami Sabbagh <rami.sab07@gmail.com>
2022-04-09 11:55:23 +0300

    Apply a minor improvement

--- src/options.ts
+++ src/options.ts
@@ -2,7 +2,7 @@ const options = {
     routerAddress: process.env.ROUTER_ADDRESS ?? '192.168.1.1',
     adminUsername: process.env.ROUTER_USERNAME ?? 'admin',
     adminPassword: process.env.ROUTER_PASSWORD ?? 'admin',
-    metricsPort: process.env.METRICS_PORT ?? 9926,
+    metricsPort: Number.parseInt(process.env.METRICS_PORT ?? '9926'),
 };
 
 export default options;
